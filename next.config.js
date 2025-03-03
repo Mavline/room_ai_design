2ecc005b7c50a914af1c7f08845f4129bc985258
Pavel Konovalov <pavelk@a-l.co.il>
2025-03-03 17:15:52 +0200

    Removed errors from logs

--- next.config.js
+++ next.config.js
@@ -17,4 +17,11 @@ module.exports = {
     }
     return config;
   },
+  // Disable automatic preloading of resources
+  onDemandEntries: {
+    // Period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // Number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
+  },
 };
